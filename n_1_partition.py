480fe032e49e5a9329c52b7c70f0db47a3b06070
Uchada1124 <uchidadaiki1124@gmail.com>
2024-10-18 13:02:50 +0900

    add [1, n-1] partition

--- n_1_partition.py
+++ n_1_partition.py
@@ -1,18 +1,40 @@
 import core_utils
 import partition_wise_pessimistic_core
+import coalition_wise_pessimistic_core 
 
 def main():
-    n = 7
-    data = list(range(1, n+1))
-    subsets = core_utils.get_subsets(data)
-    cnt = 0
-    target = [[1],[2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]]
-    for subset in subsets:
-            result = partition_wise_pessimistic_core.check_partition_wise_pessimistic_deviation(target, subset)
-            print(f"Result: {result}")
-            if result: cnt+=1
+    start = 3
+    end = 12
 
-    print(cnt)
+    for n in range(start, end + 1):
+        print(f"\nChecking for n = {n}")
+        data = list(range(1, n+1))
+        target = [[1], list(range(2, n+1))]
+        subsets = core_utils.get_subsets(data)
+
+        partition_is_core = True
+        coalition_is_core = True
+
+        for subset in subsets:
+            if partition_is_core:
+                partition_wise_result = partition_wise_pessimistic_core.check_partition_wise_pessimistic_deviation(target, subset)
+            if coalition_is_core:
+                coalition_wise_result = coalition_wise_pessimistic_core.check_coalition_wise_pessimistic_deviation(target, subset)
+            
+            if partition_wise_result:
+                partition_is_core = False
+            if coalition_wise_result:
+                coalition_is_core = False
+            
+            if not partition_is_core and not coalition_is_core:
+                print(f"The 1, n-1 partition for {n} players:")
+                print(f"  coalition-wise: False")
+                print(f"  partition-wise: False")
+                break
+        else:
+            print(f"The 1, n-1 partition for {n} players:")
+            print(f"  coalition-wise: {coalition_is_core}")
+            print(f"  partition-wise: {partition_is_core}")
 
 if __name__ == '__main__':
-      main()
+    main()
